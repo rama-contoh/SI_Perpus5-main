a5e214b7c63d93f6ec10a2ace7e4998da24301c8
rama <ramaalfi7673@gmail.com>
2023-12-09 17:37:59 +0700

    first commit

---
+++ app/Models/Book.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+
+class Book extends Model
+{
+    use HasFactory;
+    public function bookshelf(): BelongsTo
+    {
+        return $this->belongsTo(Bookshelf::class);
+    }
+
+    protected $fillable = [
+        'title',
+        'author',
+        'year',
+        'publisher',
+        'city',
+        'quantity',
+        'bookshelf_id',
+        'cover',
+    ];
+
+    public static function getDataBooks()
+    {
+        $books = Book::all();
+        $books_filter = [];
+
+        $no = 1;
+        for ($i=0; $i < $books->count(); $i++) {
+            $books_filter[$i]['no'] = $no++;
+            $books_filter[$i]['title'] = $books[$i]->title;
+            $books_filter[$i]['author'] = $books[$i]->author;
+            $books_filter[$i]['year'] = $books[$i]->year;
+            $books_filter[$i]['publisher'] = $books[$i]->publisher;
+        }
+        return $books_filter;
+    }
+
+}
---
+++ routes/web.php
@@ -0,0 +1,61 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use GuzzleHttp\Middleware;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware('auth')->group(function () {
+    // Route::get('/role', function () {
+    //     return view('welcome');
+    // })->middleware(['role:mahasiswa']);
+    Route::view('/roles', 'role')->name('role')->middleware(['role:admin']);
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/books', [BookController::class, 'index'])->name('book');
+    Route::get('/books/create', [BookController::class, 'create'])->name('book.create');
+    Route::post('/books', [BookController::class, 'store'])->name('book.store');
+    Route::get('/books/{id}/edit', [BookController::class,'edit'])->name('book.edit');
+    Route::match(['put', 'patch'], '/books/{id}',[BookController::class, 'update'])->name('book.update');
+    Route::delete('/books/{id}', [BookController::class,'destroy'])->name('book.destroy');
+    Route::get('/books/print', [BookController::class, 'print'])->name('book.print');
+    Route::get('/books/export', [BookController::class,'export'])->name('book.export');
+    Route::post('/books/import', [BookController::class,'import'])->name('book.import');
+
+
+});
+
+
+
+
+
+
+require __DIR__ . '/auth.php';
